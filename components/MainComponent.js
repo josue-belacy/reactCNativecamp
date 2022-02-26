4c2656bef67b1a3669a79827e9eaf29286e5b837
Josue Belacy <jbelacy@gmail.com>
2022-02-25 21:10:08 -0500

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import { Campsites } from '../shared/campsites';
-import { render } from 'react-dom';
+import CampsiteInfo  from './CampsiteInfoComponent';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation';
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory},
+        CampsiteInfo: {screen: CampsiteInfo}
+    },
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state ={
-      campsites: Campsites
-    };
-  }
-
-
-  render() {
-    return <Directory campsites={this.state.campsites} />;
-  }
+    render() {
+        return (
+            <View 
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0: Constants.statusBarHeight 
+                }}
+            >
+                <AppNavigator />
+            </View>
+        );
+    }
 }
+
 
 export default Main;
