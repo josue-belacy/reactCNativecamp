039d2089df8cf7e366beb763fb372a9f332d74d9
Josue Belacy <jbelacy@gmail.com>
2022-03-17 21:57:45 -0400

    import stylesheet into favorites solves issue

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,6 +4,7 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+// import { StyleSheet } from 'react-native';
 import { SwipeRow } from 'react-native-swipe-list-view';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { deleteFavorite } from '../redux/ActionCreators';
