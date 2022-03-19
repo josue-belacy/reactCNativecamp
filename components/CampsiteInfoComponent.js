9ba5d781256003a42c22d584bd8160b04e08517c
Josue Belacy <jbelacy@gmail.com>
2022-03-19 03:04:39 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl';
@@ -62,19 +62,20 @@ function RenderCampsite(props) {
             return true;
         }
     });
+
     const shareCampsite = (title, message, url) => {
         Share.share({
             title: title,
             message: `${title}: ${message} ${url}`,
             url: url
         },{
-            dialogTitle: 'Share' + title
+            dialogTitle: 'Share ' + title
         });
-    };  
+    };
 
     if (campsite) {
         return (
-<Animatable.View
+            <Animatable.View
                 animation='fadeInDown'
                 duration={2000}
                 delay={1000}
@@ -93,14 +94,27 @@ function RenderCampsite(props) {
                             color='#f50'
                             raised
                             reverse
+                            onPress={() =>
+                                props.favorite
+                                ? console.log('Already set as a favorite')
+                                : props.markFavorite()
+                            }
                         />
                         <Icon
-                            onPress={() => props.favorite ? console.log('already favorite') : props.markFavorite()}
                             name='pencil'
                             type='font-awesome'
                             color='#5637DD'
                             raised
                             reverse
+                            onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                         />
                     </View>
                 </Card>
